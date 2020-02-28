dcc247320b147f7fbe3ec063986d466db4b4f3dd
Driver <dricodp@dunwoody.edu>
2020-02-27 20:02:45 -0600

    pusing up assignment 1

--- Program.cs
+++ Program.cs
@@ -2,61 +2,72 @@
 
 namespace assignment_one
 {
-    class estimate
+    public class program
     {
         public static void Main(string[] args)
-        {   
-           //Getting Owners Name
-           string dog_owner;
-           Console.Write("Enter Owners Name:  ");
-           dog_owner = Console.ReadLine();
+        {  
+
+           estimate one = new estimate(); 
+ 
+           Console.WriteLine("Enter Owners Name:  ");
+           one.dog_owner = Console.ReadLine();
            
            //Getting Dogs Name 
-           string dog_name; //Declaring Object
            Console.Write("Enter Dogs Name: "); //asking for user input
-           dog_name = Console.ReadLine(); // getting user input
+           one.dog_name = Console.ReadLine(); // getting user input
            
            //Getting Dogs Weight
-           string dog_weight;
            Console.Write("Enter Dogs Weight: ");
-           dog_weight = Console.ReadLine();
+           one.dog_weight = Console.ReadLine();
 
            //Getting Days Of stay
-           string dog_stay;
-           int dog_nights;     
+    
            Console.Write("Please Enter The number of days the dog is staying:  ");
-           dog_stay = Console.ReadLine();
-           dog_nights = Convert.ToInt32(dog_stay);
+           one.dog_stay = Console.ReadLine();
+           one.dog_nights = Convert.ToInt32(one.dog_stay);
 
            //Getting User Code
-           string dog_code;
            Console.WriteLine(" No Add-On Code N: $75 Per Night | Bathing And Grooming Code A: $169 Per Night | Bathing Only Code C: $112 Dollars Per Night ");
            Console.Write("Please Enter the Code you wish to Recieve: ( N | A | C ):   ");
-           dog_code = Console.ReadLine();
+           one.dog_code = Console.ReadLine();
 
-           // Declaring Total Amount 
-           int total;
-
-           if (dog_code == "N"){
-               total = 75 * dog_nights;
-               Console.WriteLine("Your Total Estimated Amount is '{0}'", total.ToString());
+           if (one.dog_code == "N"){
+               one.total = 75 * one.dog_nights;
+               Console.WriteLine("Your Total Estimated Amount is '{0}'", one.total.ToString());
            }
 
-           else if (dog_code == "A"){
-               total = 169 * dog_nights;
-               Console.WriteLine("Your Total Estimated Amount is '{0}'", total.ToString());
+           else if (one.dog_code == "A"){
+               one.total = 169 * one.dog_nights;
+               Console.WriteLine("Your Total Estimated Amount is '{0}'", one.total.ToString());
            }
 
-           else if (dog_code == "C"){
-               total = 112 * dog_nights;
-               Console.WriteLine("Your Total Estimated Amount is '{0}'", total.ToString());
+           else if (one.dog_code == "C"){
+               one.total = 112 * one.dog_nights;
+               Console.WriteLine("Your Total Estimated Amount is '{0}'", one.total.ToString());
            }
 
-           else if (dog_code != "N" || dog_code != "A" || dog_code != "C" ){
+           else if (one.dog_code != "N" || one.dog_code != "A" || one.dog_code != "C" ){
                Console.WriteLine("Your Total Estimated Amount is Invalid, Because Code Does Not exist, Sorry!");
            }
 
 
         }
+
+    }
+    public class estimate
+    {
+        public string dog_owner {get; set;}
+
+        public string dog_name {get; set;}
+
+        public string dog_weight {get; set;}
+
+        public int dog_nights {get; set;}
+
+        public string dog_stay {get; set;}
+
+        public string dog_code {get; set;}
+
+        public int total {get; set;}
     }
 }
